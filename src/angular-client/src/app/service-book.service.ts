24185285ab5508855667bf3949fd6c0ff845142b
opreaandrei <andrei_s_oprea@yahoo.com>
2022-12-03 17:45:30 +0200

    added service and home component

---
+++ src/angular-client/src/app/service-book.service.ts
@@ -0,0 +1,49 @@
+import { EnvironmentInjector, Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, of, tap } from 'rxjs';
+import { Book } from './models/book';
+
+import {environment} from"../../src/environments/environment";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceBookService {
+
+  private server = environment.apiUrl+ "/api/v1/books";
+
+
+constructor(private http:HttpClient){
+    this.getBooks().subscribe();
+}
+
+
+getBooks():Observable<Array<Book>>{
+    return this.http.get<Array<Book>>(this.server+"/books").pipe(
+      catchError(this.handleError)
+    );
+}
+
+
+private handleError(error: HttpErrorResponse): Observable<any>{
+
+  console.log(error);
+
+  let errorMessage: string;
+  if(error.error instanceof ErrorEvent){
+    errorMessage =`Client Error - ${error.error.message}`;
+
+  }else{
+
+    if(error.error.reason){
+      errorMessage = `${error.error.reason} - Error code ${error.status}`;
+    } else {
+      errorMessage = `An error occurred - Error code ${error.status}`;
+    }
+  }
+  return of(errorMessage);
+
+}
+
+
+}
--- src/main/java/ro/mycode/crudmodel/constans/Util.java
+++ src/main/java/ro/mycode/crudmodel/constans/Util.java
@@ -11,6 +11,6 @@ public class Util {
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[] PUBLIC_URLS = { "/user/login", "/user/register", "/user/image/**" };
-    //public static final String[] PUBLIC_URLS = { "**" };
+    public static final String[] PUBLIC_URLS = { "/user/login", "/user/register", "/user/image/**" ,"/api/v1","/api/v1/books/**" };
+//    public static final String[] PUBLIC_URLS = { "**" };
 }
